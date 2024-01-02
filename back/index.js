f851f620ead1249a95154bf8d315f3858d7f7530
j1rrod <jarrod3917@gmail.com>
2024-01-02 22:15:12 +0300

    add

--- back/index.js
+++ back/index.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const app = express()
-
+import express from 'express'
+// import cors from 'cors'
+const app = express();
 app.use(express.json())
 app.get('/', (req, res) =>{
     res.send('Hello World')
